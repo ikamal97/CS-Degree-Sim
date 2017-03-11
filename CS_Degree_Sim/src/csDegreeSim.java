b4fc50c36f15cc46df61e86e1c7862bbb4caf410
JP9667 <jp@jpbackman.com>
2017-03-11 15:48:32 -0600

    Add files via upload

--- CS_Degree_Sim/src/csDegreeSim.java
+++ CS_Degree_Sim/src/csDegreeSim.java
@@ -1,99 +1,111 @@
-import java.util.ArrayList;
+import java.util.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
 public class csDegreeSim{
 	
 	public static void main(String[] args){
-		
+		csDegreeSim CSDegreeSimulator = new csDegreeSim();
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            	CSDegreeSimulator.start();
+            }
+        });
+
 	}
 	
 	public csDegreeSim(){
-		
+		//constructor
 	}
-	
-	public interface Subject{
-		public void registerObserver(Observer o);
-		public void removeObserver(Observer o);
-		public void notifyObservers();
+		
+	private void start(){
+		//new mainMenu/Game/etc...
 	}
-	
-	public interface Observer{
-		public void update(int stats);
-	}
-	
-	public interface DisplayElement{
-		public void display();
-	}
-	
-	public class Game implements Subject{
-		private ArrayList<Observer> observers;
-		
-		public Game(){
-			observers = new ArrayList<Observer>();
-		}
-		
-		public void registerObserver(Observer o){
-			observers.add(o);
-		}
-		
-		public void removeObserver(Observer o){
-			int index = observers.indexOf(o);
-			if(index >= 0)
-				observers.remove(index);
-		}
-		
-		public void notifyObservers(){
-			
-		}
-		
-	}
-	
-	public class Character implements Subject{
-		private ArrayList<Observer> observers;
-		
-		public Character(){
-			observers = new ArrayList<Observer>();
-		}
-		
-		public void registerObserver(Observer o){
-			observers.add(o);
-		}
-		
-		public void removeObserver(Observer o){
-			int index = observers.indexOf(o);
-			if(index >= 0)
-				observers.remove(index);
-		}
-		
-		public void notifyObservers(){
-			
-		}
-		
-	}
-	
-	public class mainMenu implements Observer, DisplayElement{
-		private int screenStats;
-		private Subject Character;
-		
-		public mainMenu(Subject Character){
-			this.Character = Character;
-			Character.registerObserver(this);
-		}
-		
-		public void update(int stats){
-			this.screenStats = stats;
-			display();
-		}
-		
-		public void display(){
-			//code for updating display
-		}
-			
-	}
-	
-	
 	
 	
 }
 
+
+
+interface Subject{
+	public void registerObserver(Observer o);
+	public void removeObserver(Observer o);
+	public void notifyObservers();
+}
+
+interface Observer{
+	public void update(int stats);
+}
+
+interface DisplayElement{
+	public void display();
+}
+
+class Game implements Subject{
+	private ArrayList<Observer> observers;
+	
+	public Game(){
+		observers = new ArrayList<Observer>();
+	}
+	
+	public void registerObserver(Observer o){
+		observers.add(o);
+	}
+	
+	public void removeObserver(Observer o){
+		int index = observers.indexOf(o);
+		if(index >= 0)
+			observers.remove(index);
+	}
+	
+	public void notifyObservers(){
+		
+	}
+	
+}
+
+class Character implements Subject{
+	private ArrayList<Observer> observers;
+	
+	public Character(){
+		observers = new ArrayList<Observer>();
+	}
+	
+	public void registerObserver(Observer o){
+		observers.add(o);
+	}
+	
+	public void removeObserver(Observer o){
+		int index = observers.indexOf(o);
+		if(index >= 0)
+			observers.remove(index);
+	}
+	
+	public void notifyObservers(){
+		
+	}
+	
+}
+
+class mainMenu implements Observer, DisplayElement{
+	private int screenStats;
+	private Subject Character;
+	
+	public mainMenu(Subject Character){
+		this.Character = Character;
+		Character.registerObserver(this);
+	}
+	
+	public void update(int stats){
+		this.screenStats = stats;
+		display();
+	}
+	
+	public void display(){
+		//code for updating display
+	}
+		
+}
