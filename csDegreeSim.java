845cfc08d312238e85e90afa4534c3d56da5fdda
eemmanuel91 <eemmanuel1991@gmail.com>
2017-03-12 20:51:05 -0500

    Update csDegreeSim.java

--- csDegreeSim.java
+++ csDegreeSim.java
@@ -2,18 +2,21 @@ package csDegreeSim2;
 
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
 
-public class csDegreeSim {
-	
+
+public class csDegreeSim{
+	public static int Height = 400;
+	public static int Width = 600;
+	JPanel panel;
+	JMenuBar mainMenu;
+	public JFrame frame;
 	
 	
 	public static void main(String[] args){
 		csDegreeSim CSDegreeSimulator = new csDegreeSim();
-	
-		
-		
 		SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -25,11 +28,33 @@ public class csDegreeSim {
 	
 	public csDegreeSim(){
 		//constructor
+		frame = new JFrame();
+		frame.setTitle("CS Degree Simulator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setSize(new Dimension(Width, Height));
+		frame.setLocation(200, 200);
 	}
 		
 	private void start(){
 		//new mainMenu/Game/etc...
+		//setPanel(new MainMenu(this));
 	}
+	
+	public JFrame getJFrame(){
+		return frame;
+	}
+	
+	void setPanel(JPanel newPanel) {
+		frame.add(newPanel);
+		if (panel != null)
+			frame.remove(panel);
+		panel = newPanel;
+		frame.setVisible(true);
+	}
+	
 	
 }
 
@@ -48,7 +73,7 @@ interface DisplayElement{
 	public void display();
 }
 
-class Game implements Subject{
+class Game implements Subject  {
 	private ArrayList<Observer> observers;
 	
 	public Game(){
@@ -66,6 +91,41 @@ class Game implements Subject{
 	}
 	
 	public void notifyObservers(){
+		
+	}
+	
+	public int randomNumberGenerator(){
+		Random RNG = new Random();
+		int randomNumber = RNG.nextInt(100); //0-100
+		return randomNumber;
+	}
+	public int randomEventSelector (){
+		Random randomNumGenerator=new Random();
+		int randomNumber=randomNumGenerator.nextInt(100); //0-100
+		
+		return randomNumber;
+		
+	}
+	public abstract class Event{
+	
+	
+	 int number=g1.randomEventSelector();
+		
+		if(randomNumber >=0 && randomNumber <= 50){
+			System.out.println("Good Event");
+			//set Good event
+		}
+		else if(randomNumber > 50 && randomNumber <= 100){
+		    System.out.println("Bad Event");
+		     //set Bad event 
+		    //not soo sure how to put it together  
+		}
+	}
+	
+	public class goodEvent extends Event{
+		
+	}
+	public class badEvent extends Event{
 		
 	}
 	
@@ -101,16 +161,75 @@ class Character implements Subject{
 	}
 	
 }
-class MainMenu implements Observer, DisplayElement{
+
+class MainMenu extends JPanel implements Observer, DisplayElement{
 	private int screenStats;
 	private Subject Character;
-	private JLabel label;
+	private JPanel panel;
+	private csDegreeSim sim;
+	private JTextField playerName;
 	
+	public MainMenu(csDegreeSim sim){
+		this.sim = sim;
+		setBackground(Color.WHITE);
+		setLayout(new BorderLayout(0, 0));
+		setSize(new Dimension(600, 350));
+		
+		JPanel headerPanel = new JPanel();
+		headerPanel.setBackground(Color.white);
+		add(headerPanel, BorderLayout.NORTH);
+
+		JLabel headerLabel = new JLabel("Main Menu");
+		headerPanel.add(headerLabel);
+		
+		JPanel bodyPanel = new JPanel();
+		bodyPanel.setBackground(Color.BLUE);
+		add(bodyPanel, BorderLayout.CENTER);
+		bodyPanel.setLayout(null);
+		
+		JButton newGame = new JButton("NEW GAME");
+		newGame.setFont(new Font("Dialog", Font.BOLD, 30));
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+		newGame.setBackground(Color.GREEN);
+		newGame.setForeground(Color.WHITE);
+		newGame.setBounds(210, 20, 170, 30);
+		newGame.setBorder(border);
+		bodyPanel.add(newGame);
+		
+		JButton loadGame = new JButton("LOAD GAME");
+		loadGame.setFont(new Font("Dialog", Font.BOLD, 30));
+		Border border2 = BorderFactory.createLineBorder(Color.BLACK, 1);
+		loadGame.setBackground(Color.GREEN);
+		loadGame.setForeground(Color.WHITE);
+		loadGame.setBounds(205, 90, 183, 30);
+		loadGame.setBorder(border2);
+		bodyPanel.add(loadGame);
+		
+		JButton options = new JButton("OPTIONS");
+		options.setFont(new Font("Dialog", Font.BOLD, 30));
+		Border border3 = BorderFactory.createLineBorder(Color.BLACK, 1);
+		options.setBackground(Color.GREEN);
+		options.setForeground(Color.WHITE);
+		options.setBounds(215, 160, 136, 30);
+		options.setBorder(border3);
+		bodyPanel.add(options);
+		
+		JButton exit = new JButton("EXIT");
+		exit.setFont(new Font("Dialog", Font.BOLD, 30));
+		Border border4 = BorderFactory.createLineBorder(Color.BLACK, 1);
+		exit.setBackground(Color.GREEN);
+		exit.setForeground(Color.WHITE);
+		exit.setBounds(255, 230, 70, 30);
+		exit.setBorder(border4);
+		bodyPanel.add(exit);
+		
+
+	}
 	
-	public MainMenu(Subject Character){//recieves reference to subject
+	/*public MainMenu(Subject Character){//recieves reference to subject
 		this.Character = Character;
 		Character.registerObserver(this);//registers this observer with subject
-	}
+	}*/
 	
 	public void update(int stats){
 		this.screenStats = stats;
@@ -119,35 +238,11 @@ class MainMenu implements Observer, DisplayElement{
 	
 	public void display(){
 		//code for updating display
-		JFrame window= new JFrame("Main Menu");
-		JPanel panel=new JPanel();
-		JLabel label=new JLabel();
-		JButton newGame=new JButton("New Game");
-		JButton loadGame=new JButton("Load Game");
-		JButton selectMoral=new JButton("Select Moral");
-		JButton selectCharacter=new JButton("Select Character");
-		JButton saveGame=new JButton("Save Game");
 		
-		window.setVisible(true);
-		window.setSize(1000,700);
-		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		panel.setBackground(Color.GRAY);
-		
-		panel.add(newGame);
-		panel.add(loadGame);
-		panel.add(saveGame);
-		panel.add(selectMoral);
-		panel.add(selectCharacter);
-		
-		panel.add(label);
-		
-		window.add(panel,BorderLayout.SOUTH);
-	    
-	//  newGame.addActionListener(this);
-	//	newGame.addKeyListener(this);
-	
 	}
 		
 }
+
+
+
+
