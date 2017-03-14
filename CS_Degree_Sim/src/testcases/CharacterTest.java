b2b15e8ac45f37584a9a52aece81377aa3b9dece
eemmanuel91 <eemmanuel1991@gmail.com>
2017-03-13 22:13:28 -0500

    Update CharacterTest.java

--- CS_Degree_Sim/src/testcases/CharacterTest.java
+++ CS_Degree_Sim/src/testcases/CharacterTest.java
@@ -18,5 +18,19 @@ public class CharacterTest {
 		character.damageEnergy(10);
 		assertEquals(90, character.getEnergy());
 	}
+        @Test
+	public void increaseMoraleTest() {
+		Character character = new Character();
+		character.increaseMorale(10);
+		assertEquals(110, character.getMorale());
+		
+	}
+	
+	@Test
+	public void increaseEnergyTest() {
+		Character character = new Character();
+		character.increaseEnergy(10);
+		assertEquals(110, character.getEnergy());
+	}
 
 }
