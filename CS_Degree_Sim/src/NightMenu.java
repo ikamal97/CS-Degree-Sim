28e25190f3c25e1c0a42ee1251fcc6debd96ab2f
Idrees Kamal <ikamal97@gmail.com>
2017-04-03 01:41:09 -0500

    Changed the text color in the night menu from black to white... better contrast

--- CS_Degree_Sim/src/NightMenu.java
+++ CS_Degree_Sim/src/NightMenu.java
@@ -7,14 +7,14 @@ import java.awt.event.*;
 public class NightMenu extends JPanel implements Observer, DisplayMenu {
 
 	private Character character;
-	private int moral, energy, intelligence, endurance, charisma;
+	private int morale, energy, intelligence, endurance, charisma;
 	private String characterName;
 	private csDegreeSim sim;
 	private Game game;
 	private int button = 0;
 	private Image background;
 	private JPanel bodyPanel, headerPanel;
-	private JLabel headerLabel, timerLabel, moralLabel, energyLabel, nameLabel;
+	private JLabel headerLabel, timerLabel, moraleLabel, energyLabel, nameLabel;
 	private JLabel intelligenceLabel, enduranceLabel, charismaLabel, backgroundLabel; 
 	private JButton startGame, plusINT, plusEND, plusCHR;
 	private JButton quit, nextDay;
@@ -52,9 +52,9 @@ public class NightMenu extends JPanel implements Observer, DisplayMenu {
 	public void createComponents() {
 		nextDay = new JButton("NEXT DAY");
 		nextDay.setFont(new Font("Dialog", Font.BOLD, 30));
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+		Border border = BorderFactory.createLineBorder(Color.WHITE, 2);
 		nextDay.setBackground(Color.GRAY);
-		nextDay.setForeground(Color.BLACK);
+		nextDay.setForeground(Color.WHITE);
 		nextDay.setBounds(50, 340, 300, 30);
 		nextDay.setBorder(border);
 		nextDay.addActionListener(listen);
@@ -63,9 +63,9 @@ public class NightMenu extends JPanel implements Observer, DisplayMenu {
 		
 		plusINT = new JButton("STUDY: +INT(5 Energy)");
 		plusINT.setFont(new Font("Dialog", Font.BOLD, 15));
-		Border border2 = BorderFactory.createLineBorder(Color.BLACK, 2);
+		Border border2 = BorderFactory.createLineBorder(Color.WHITE, 2);
 		plusINT.setBackground(Color.GRAY);
-		plusINT.setForeground(Color.BLACK);
+		plusINT.setForeground(Color.WHITE);
 		plusINT.setBounds(50, 200, 200, 20);
 		plusINT.setBorder(border2);
 		plusINT.addActionListener(listen);
@@ -74,9 +74,9 @@ public class NightMenu extends JPanel implements Observer, DisplayMenu {
 		
 		plusEND = new JButton("LIFT: +END(8 Energy)");
 		plusEND.setFont(new Font("Dialog", Font.BOLD, 15));
-		Border border3 = BorderFactory.createLineBorder(Color.BLACK, 2);
+		Border border3 = BorderFactory.createLineBorder(Color.WHITE, 2);
 		plusEND.setBackground(Color.GRAY);
-		plusEND.setForeground(Color.BLACK);
+		plusEND.setForeground(Color.WHITE);
 		plusEND.setBounds(50, 230, 200, 20);
 		plusEND.setBorder(border3);
 		plusEND.addActionListener(listen);
@@ -85,9 +85,9 @@ public class NightMenu extends JPanel implements Observer, DisplayMenu {
 		
 		plusCHR = new JButton("DRINK: +CHR(4 Energy)");
 		plusCHR.setFont(new Font("Dialog", Font.BOLD, 15));
-		Border border4 = BorderFactory.createLineBorder(Color.BLACK, 2);
+		Border border4 = BorderFactory.createLineBorder(Color.WHITE, 2);
 		plusCHR.setBackground(Color.GRAY);
-		plusCHR.setForeground(Color.BLACK);
+		plusCHR.setForeground(Color.WHITE);
 		plusCHR.setBounds(50, 260, 200, 20);
 		plusCHR.setBorder(border4);
 		plusCHR.addActionListener(listen);
@@ -100,11 +100,11 @@ public class NightMenu extends JPanel implements Observer, DisplayMenu {
 		nameLabel.setBounds(50, 20, 300, 22);
 		bodyPanel.add(nameLabel);
 		
-		moralLabel = new JLabel("Moral: " + moral);
-		moralLabel.setFont(new Font("Dialog", Font.BOLD, 18));
-		moralLabel.setForeground(Color.WHITE);
-		moralLabel.setBounds(50, 50, 176, 18);
-		bodyPanel.add(moralLabel);
+		moraleLabel = new JLabel("Morale: " + morale);
+		moraleLabel.setFont(new Font("Dialog", Font.BOLD, 18));
+		moraleLabel.setForeground(Color.WHITE);
+		moraleLabel.setBounds(50, 50, 176, 18);
+		bodyPanel.add(moraleLabel);
 		
 		energyLabel = new JLabel("Energy: " + energy);
 		energyLabel.setFont(new Font("Dialog", Font.BOLD, 18));
@@ -137,15 +137,15 @@ public class NightMenu extends JPanel implements Observer, DisplayMenu {
 
 	public void initialCharacterInfo(){
 		this.characterName = character.getName();
-		this.moral = character.getMorale();
+		this.morale = character.getMorale();
 		this.energy = character.getEnergy();
 		this.intelligence = character.getInt();
 		this.endurance = character.getEnd();
 		this.charisma = character.getChr();
 	}
 	
-	public void updateStats(int moral, int energy){
-		this.moral = moral;
+	public void updateStats(int morale, int energy){
+		this.morale = morale;
 		this.energy = energy;
 		
 		this.energyLabel.setText("Energy: " + energy);
