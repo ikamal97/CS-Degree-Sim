0c6f3fdd486cf73f1f28fd258e91391e5d8e0256
JP9667 <jp@jpbackman.com>
2017-04-24 22:48:17 -0500

    Added new events
    - Added new scenarios
    -Bit of balancing

--- CS_Degree_Sim/src/Event.java
+++ CS_Degree_Sim/src/Event.java
@@ -2,13 +2,13 @@ import java.awt.Image;
 import java.util.Random;
 
 public abstract class Event {
-	private Image background;
 	public String eventType;
 	public String dialog;
 	public String statLimit;
 	public int statSuccess;
 	public int successChance;
 	public int moralModifier;
+	public int energyModifier;
 	public int moralDamage;
 	public int moralIncrease;
 	public int energyDamage;
@@ -35,16 +35,24 @@ class SimpleEvent extends Event{
 	public void selectEvent(int eventNumber){
 		switch(eventNumber){
 		case 0:
-			dialog = "A Professor has extended an assignment deadline by a week.";
+			dialog = "A Professor has extended an assignment deadline by a week. (+2 moral)";
 			moralModifier = 2;
 			return;
 		case 1:
-			dialog = "Your computer crashes, losing the progress you have made on your next assignment";
+			dialog = "Your computer crashes, losing the progress you have made on your next assignment. (-4 moral)";
 			moralModifier = -4;
 			return;
 		case 2:
-			dialog = "You found an exact solution to an assignment online";
+			dialog = "You found the exact solution to an assignment online. (+2 moral)";
 			moralModifier = 2;
+			return;
+		case 3:
+			dialog = "You missed the bus and missed a lecture as a result. (-3 moral)";
+			moralModifier = -3;
+			return;
+		case 4:
+			dialog = "You tripped on your way to the next lecture. (-1 moral)";
+			moralModifier = -1;
 			return;
 		}
 	}
@@ -67,7 +75,7 @@ class ChoiceEvent extends Event{
 		case 0:
 			dialog = "A classmate claims to have the solution to a current assignment, you try to persuade him to give it to you.";
 			statLimit = "CHR";
-			statSuccess = 12;
+			statSuccess = 16;
 			successChance = 30;
 			moralIncrease = 5;
 			moralDamage = 3;
@@ -79,6 +87,30 @@ class ChoiceEvent extends Event{
 			successChance = 45;
 			moralIncrease = 2;
 			moralDamage = 3;
+			return;
+		case 2:
+			dialog = "A pop quiz happens in a lecture.";
+			statLimit = "INT";
+			statSuccess = 14;
+			successChance = 25;
+			moralIncrease = 3;
+			moralDamage = 7;
+			return;
+		case 3:
+			dialog = "You are called up to present your project design that you forgot to prepare for.";
+			statLimit = "CHR";
+			statSuccess = 20;
+			successChance = 20;
+			moralIncrease = 5;
+			moralDamage = 5;
+			return;
+		case 4:
+			dialog = "You just missed the bus but a sheet assignment needs to be turned in to the professor before the next bus arrives. You run for it.";
+			statLimit = "END";
+			statSuccess = 12;
+			successChance = 15;
+			moralIncrease = 2;
+			moralDamage = 10;
 			return;
 		}
 	}
